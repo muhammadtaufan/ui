bdb5dab1d927c6397cd28c0aab247fdaf5742428
fathurifki <rifkifathu@gmail.com>
2023-02-01 00:03:21 +0700

    add base routes and base component dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import LoginPage from "@/container/Login";
+import RouteWrapper from "@/components/RouteWrapper";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
+        <Route path="admin/*" element={<RouteWrapper />} />
+        <Route path="admin" element={<Navigate to="/dashboard" />} />
+        <Route path="admin/login" element={<Navigate to="/dashboard" />} />
+        <Route path="*" element={<Navigate to="admin/dashboard" />} />
       </Routes>
     </>
   );
